3eb48c03e5a6b0f510369541cc5b3472cbdcb5bc
Tristan Huet <tristan.huet@cosmotech.com>
2021-04-14 16:41:36 +0200

    Add translation and 'Change language' button in UserInfo

--- src/UserInfo/UserInfo.js
+++ src/UserInfo/UserInfo.js
@@ -5,7 +5,12 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { Auth } from '@cosmotech/core'
 import { Box, Menu, MenuItem, withStyles } from '@material-ui/core'
+import {
+  ArrowRight as ArrowRightIcon,
+  Check as CheckIcon
+} from '@material-ui/icons'
 import profilePlaceholder from '../../assets/profile_placeholder.png'
+import { useTranslation } from 'react-i18next'
 
 const useStyles = theme => ({
   menuTrigger: {
@@ -33,11 +38,18 @@ const useStyles = theme => ({
     textAlign: 'center',
     lineHeight: '32px',
     height: '32px',
-    marginLeft: '8px',
     marginLeft: '8px'
   },
   menu: {
     transform: 'translate3d(0,30px,0) !important'
+  },
+  menuContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  menuIcon: {
+    marginLeft: '20px'
   },
   docLink: {
     color: theme.palette.primary.contrastText
@@ -49,7 +61,10 @@ const UserInfo = (props) => {
   const [name, setName] = useState('')
   const [picUrl, setPicUrl] = useState(profilePlaceholder)
   const [isMenuOpened, setIsMenuOpened] = useState(false)
+  const [isLangMenuOpened, setLangIsMenuOpened] = useState(false)
   const [anchorEl, setAnchorEl] = useState(null)
+  const [langAnchorEl, setLangAnchorEl] = useState(null)
+  const { t, i18n } = useTranslation()
 
   useEffect(() => {
     // Bind callback to update component on authentication data change
@@ -77,13 +92,28 @@ const UserInfo = (props) => {
     setIsMenuOpened(!isMenuOpened)
   }
 
+  const handleLanguageMenuClick = (e) => {
+    setLangAnchorEl(e.target)
+    setLangIsMenuOpened(!isLangMenuOpened)
+  }
+
+  const setLanguage = (lang) => {
+    setIsMenuOpened(false)
+    setLangIsMenuOpened(false)
+    i18n.changeLanguage(lang)
+  }
+
   const { classes } = props
+  const profilePictureUrl = props.profilePictureUrl
+    ? props.profilePictureUrl
+    : profilePlaceholder
+
   return (
     <React.Fragment>
       <Box
-        aria-controls="simple-menu"
-        aria-haspopup="true"
         data-cy="user-profile-menu"
+        aria-controls="user-profile-button"
+        aria-haspopup="true"
         onClick={handleClick}
         className={`${classes.menuTrigger} ${isMenuOpened ? 'active' : ''}`}
       >
@@ -92,33 +122,82 @@ const UserInfo = (props) => {
       </Box>
       <Menu
         className={classes.menu}
-        id="simple-menu"
+        data-cy="main-menu"
         keepMounted
         anchorEl={anchorEl}
         open={isMenuOpened}
         onClose={handleClick}
       >
         {
+          props.languages
+            ? <MenuItem data-cy="change-language"
+                onClick={handleLanguageMenuClick}
+                className={classes.menuContainer}
+              >
+                { t('genericcomponent.userinfo.button.changeLanguage', 'Change language') }
+                <ArrowRightIcon className={classes.menuIcon}/>
+              </MenuItem>
+            : null
+        }
+        {
           props.documentationUrl
-            ? <MenuItem data-cy="downloadDocumentation">
+            ? <MenuItem data-cy="download-documentation">
                 <a href={props.documentationUrl}
                   className={classes.docLink}
                   target="_blank"
-                  rel="noreferrer">Download documentation</a>
+                  rel="noreferrer">
+                    {t('genericcomponent.userinfo.button.downloadDocumentation', 'Download documentation')}
+                </a>
               </MenuItem>
             : null
         }
         <MenuItem data-cy="logout" onClick={() => {
           Auth.signOut()
-        }}>Logout</MenuItem>
+        }}>{t('genericcomponent.userinfo.button.logout', 'Log out')}</MenuItem>
       </Menu>
+      {
+        props.languages
+          ? <Menu
+              className={classes.menu}
+              id="simple-menu"
+              keepMounted
+              anchorEl={langAnchorEl}
+              getContentAnchorEl={null}
+              anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+              transformOrigin={{ vertical: 'center', horizontal: 'right' }}
+              open={isLangMenuOpened}
+              onClose={handleLanguageMenuClick}
+            >
+              {
+                // Language menu items
+                Object.entries(props.languages).map(
+                  ([langKey, langLabel]) =>
+                  <MenuItem
+                    data-cy={ 'set-lang-' + langKey }
+                    key={langKey}
+                    onClick={ () => setLanguage(langKey) }
+                    className={classes.menuContainer}
+                  >
+                    {langLabel}
+                    {
+                      // Add a check mark for the currently selected language
+                      langKey === i18n.language
+                        ? <CheckIcon className={classes.menuIcon}/>
+                        : null
+                    }
+                  </MenuItem>)
+              }
+            </Menu>
+          : null
+      }
     </React.Fragment>
   )
 }
 
 UserInfo.propTypes = {
   classes: PropTypes.any,
-  documentationUrl: PropTypes.string
+  documentationUrl: PropTypes.string,
+  languages: PropTypes.objectOf(PropTypes.string)
 }
 
 export default withStyles(useStyles)(UserInfo)
