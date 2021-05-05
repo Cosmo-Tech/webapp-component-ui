b79d84a32dc17daf9b80c40b6876d3db8e36163c
Tristan Huet <tristan.huet@cosmotech.com>
2021-05-05 15:02:17 +0200

    WIP tests

--- src/UserInfo/__tests__/UserInfo.test.js
+++ src/UserInfo/__tests__/UserInfo.test.js
@@ -4,17 +4,29 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import UserInfo from '../UserInfo';
+import { createMount } from '@material-ui/core/test-utils';
 
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useState: jest.fn()
-}));
+// jest.mock('react', () => ({
+//   ...jest.requireActual('react'),
+//   useState: jest.fn()
+// }));
+
+
+
+describe('BtnIcon should match Snapshot', () => {
+  it('without hint', () => {
+    expect(renderedValue.html()).toMatchSnapshot();
+  });
+});
+
+
 
 describe('UserInfo test suite', () => {
   console.error = jest.fn();
 
   it('should log errors when no props are passed', () => {
-    renderer.create(<UserInfo />).toJSON();
+    const renderedValue = createMount()(<UserInfo />);
+    // renderer.create().toJSON();
     // expect(console.error).toHaveBeenCalledTimes(3);
   });
 
