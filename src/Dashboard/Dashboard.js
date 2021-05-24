567bb668eb3e4ceb6e15c549260ec1067316b3fc
Tristan Huet <tristan.huet@cosmotech.com>
2021-05-24 17:31:24 +0200

    Display dashboard placeholder based on scenario status, if provided

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -10,10 +10,14 @@ import {
   Typography,
   Grid
 } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
 
 const useStyles = makeStyles(theme => ({
   gridContainer: {
     height: '100%'
+  },
+  iconContainer: {
+    textAlign: 'center'
   },
   iframe: {
     display: 'block',
@@ -27,16 +31,61 @@ const useStyles = makeStyles(theme => ({
 
 const Dashboard = (props) => {
   const { t } = useTranslation();
-
   const classes = useStyles();
-
-  const { iframeTitle, url, scenarioName, scenarioId, ...otherProps } = props;
-
+  const {
+    iframeTitle,
+    url,
+    scenarioName,
+    scenarioId,
+    scenarioState,
+    ...otherProps
+  } = props;
   const formattedUrl = url.replaceAll('<ScenarioName>', scenarioName).replaceAll('<ScenarioId>', scenarioId);
+
+  const getPlaceHolder = (label_key, default_label, icon) => {
+    return <Grid container justify="center" alignItems="center" className={classes.gridContainer}>
+      <Grid item>
+        { icon !== undefined &&
+          <div className={classes.iconContainer}>
+            {icon}
+          </div>
+        }
+        <Typography
+          component="h2"
+          color="textSecondary"
+          className={classes.label}
+        >
+          {t(label_key, default_label)}
+        </Typography>
+      </Grid>
+    </Grid>
+  }
+
+  // Handle optional status property
+  const noRun = scenarioState === 'Created';
+  const runInProgress = scenarioState === 'Running';
+  const hasError = scenarioState === 'Failed';
+  const isReady = scenarioState === undefined || scenarioState === 'Successful';
 
   return (
     <>
-      { formattedUrl !== '' &&
+      {
+        noRun && getPlaceHolder(
+          'commoncomponents.iframe.scenario.results.text.uninitialized',
+          'The scenario has not been run yet')
+      }
+      {
+        runInProgress && getPlaceHolder(
+          'commoncomponents.iframe.scenario.results.text.running',
+          'Scenario run in progress...',
+          <AccessTimeIcon color="primary" fontSize="large"/>)
+      }
+      {
+        hasError && getPlaceHolder(
+          'commoncomponents.iframe.scenario.results.text.error',
+          'An error occured during the scenario run')
+      }
+      { isReady && formattedUrl !== '' &&
         <iframe
           className={classes.iframe}
           title={iframeTitle}
@@ -45,18 +94,9 @@ const Dashboard = (props) => {
           src={formattedUrl} {...otherProps}
         />
       }
-      { formattedUrl === '' &&
-        <Grid container justify="center" alignItems="center" className={classes.gridContainer}>
-          <Grid item>
-            <Typography
-              component="h2"
-              color="textSecondary"
-              className={classes.label}
-            >
-              {t('commoncomponents.iframe.scenario.results.text.no.result', 'No dashboards for this scenario.')}
-            </Typography>
-          </Grid>
-        </Grid>
+      { isReady && formattedUrl === '' && getPlaceHolder(
+          'commoncomponents.iframe.scenario.results.text.no.result',
+          'No dashboards for this scenario.')
       }
     </>
   );
@@ -66,7 +106,8 @@ Dashboard.propTypes = {
   iframeTitle: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
   scenarioName: PropTypes.string,
-  scenarioId: PropTypes.string
+  scenarioId: PropTypes.string,
+  scenarioState: PropTypes.string
 };
 
 export default withStyles(useStyles)(Dashboard);
