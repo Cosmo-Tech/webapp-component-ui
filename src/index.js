1a2cbb51b463300a36b208398b16400cd3716a3c
Tristan Huet <tristan.huet@cosmotech.com>
2021-06-10 11:16:10 +0200

    feat: add CreateScenarioButton component
    This code has been moved from azure-sample-webapp repository

--- src/index.js
+++ src/index.js
@@ -13,3 +13,4 @@ export { default as BasicDateInput } from './BasicInputs/BasicDateInput';
 export { default as SimpleTwoActionsDialog } from './SimpleTwoActionsDialog';
 export { default as Dashboard } from './Dashboard';
 export { default as HierarchicalComboBox } from './HierarchicalComboBox';
+export { default as CreateScenarioButton } from './CreateScenarioButton';
