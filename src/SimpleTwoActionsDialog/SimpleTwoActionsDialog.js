8ad5396a54fbbfa56e1bc58a09b7895d19c19e8f
jreynard-code <jeremy.reynard@cosmotech.com>
2021-09-23 16:05:27 +0200

    feat:  Initialize DocZ for UI
    Add simple project description in README.md
    Add docz devDependencies (and used dependencies to devDependencies)
    Update yarn.lock
    Add .docz folder to .gitignore
    Add first component documentation (SimpleTwoActionsDialog)

--- src/SimpleTwoActionsDialog/SimpleTwoActionsDialog.js
+++ src/SimpleTwoActionsDialog/SimpleTwoActionsDialog.js
@@ -13,7 +13,7 @@ import {
   DialogActions
 } from '@material-ui/core';
 
-const SimpleTwoActionsDialog = ({
+export const SimpleTwoActionsDialog = ({
   open,
   dialogTitleKey,
   dialogBodyKey,
@@ -63,13 +63,50 @@ const SimpleTwoActionsDialog = ({
 };
 
 SimpleTwoActionsDialog.propTypes = {
+  /**
+   *  ## Define the SimpleTwoActionsDialog's state:
+   *  - true : the dialog is opened
+   *  - false : the dialog is closed
+   */
   open: PropTypes.bool.isRequired,
+  /**
+   *  ## The translation key for dialog title
+   *  We used **react-i18next** for translation.
+   *  You can referred to desired sentence by specifing the json path of translation files.
+   *
+   *  **e.g**: 'commoncomponents.dialog.mywonderfuldialog.title'
+   */
   dialogTitleKey: PropTypes.string.isRequired,
+  /**
+   *  ## The translation key for dialog body:
+   *  We used react-i18next for translation.
+   *  You can referred to desired sentence by specifing the json path of translation files.
+   *
+   *  **e.g**: 'commoncomponents.dialog.mywonderfuldialog.body'
+   */
   dialogBodyKey: PropTypes.string.isRequired,
+  /**
+   *  ## The translation key for cancel button:
+   *  We used react-i18next for translation.
+   *  You can referred to desired sentence by specifing the json path of translation files.
+   *
+   *  **e.g**: 'commoncomponents.dialog.mywonderfuldialog.button.cancel'
+   */
   cancelLabelKey: PropTypes.string.isRequired,
+  /**
+   *  ## Function used when cancel button is clicked
+   */
   handleClickOnCancel: PropTypes.func.isRequired,
+  /**
+   *  ## The translation key for validate button:
+   *  We used react-i18next for translation.
+   *  You can referred to desired sentence by specifing the json path of translation files.
+   *
+   *  **e.g**: 'commoncomponents.dialog.mywonderfuldialog.button.validate'
+   */
   validateLabelKey: PropTypes.string.isRequired,
+  /**
+   *  ## Function used when validate button is clicked
+   */
   handleClickOnValidate: PropTypes.func.isRequired
 };
-
-export default SimpleTwoActionsDialog;
