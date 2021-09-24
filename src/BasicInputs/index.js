1f261fc8ea7100ac2e2d7169ce44a7d82b96016f
jreynard-code <jeremy.reynard@cosmotech.com>
2021-09-24 16:33:52 +0200

    feat:  Add doc for BasicInputs > BasicNumberInput

--- src/BasicInputs/index.js
+++ src/BasicInputs/index.js
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 export { default as BasicTextInput } from './BasicTextInput';
-export { default as BasicNumberInput } from './BasicNumberInput';
-export { default as BasicEnumInput } from './BasicEnumInput';
+export { BasicNumberInput } from './BasicNumberInput';
+export { BasicEnumInput } from './BasicEnumInput';
 export { default as BasicToggleInput } from './BasicToggleInput';
 export { default as BasicDateInput } from './BasicDateInput';
