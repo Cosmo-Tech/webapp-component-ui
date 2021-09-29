a149c7514f8791776e3822d2ee13fb308deca279
jreynard-code <jeremy.reynard@cosmotech.com>
2021-09-29 11:07:53 +0200

    docs: Modify props pattern of CreateScenarioButton to match with ui package

--- src/CreateScenarioButton/CreateScenarioButton.js
+++ src/CreateScenarioButton/CreateScenarioButton.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Tooltip } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
-import { useTranslation } from 'react-i18next';
 import CreateScenarioDialog from './components';
 
 export const CreateScenarioButton = ({
@@ -15,11 +14,9 @@ export const CreateScenarioButton = ({
   workspaceId,
   solution,
   disabled,
-  buttonTooltip,
   nameValidator,
-  dialogLabels
+  labels
 }) => {
-  const { t } = useTranslation();
   const [open, setOpen] = useState(false);
   const openDialog = () => setOpen(true);
   const closeDialog = () => setOpen(false);
@@ -30,7 +27,7 @@ export const CreateScenarioButton = ({
 
   return (
     <div>
-      <Tooltip arrow title={buttonTooltip}>
+      <Tooltip arrow title={labels.button.tooltip}>
         <div>
           <Button
             data-cy="create-scenario-button"
@@ -41,7 +38,7 @@ export const CreateScenarioButton = ({
             color="primary"
             disabled={disabled}
           >
-            {t('commoncomponents.button.create.scenario.label', 'Create new Scenario')}
+            {labels.button.title}
           </Button>
         </div>
       </Tooltip>
@@ -58,25 +55,96 @@ export const CreateScenarioButton = ({
         user={user}
         nameValidator={nameValidator}
         datasetsFilter={datasetsFilter}
-        labels={dialogLabels}
+        dialogLabels={labels.dialog}
+        errorLabels={labels.errors}
       />
     </div>
   );
 };
 
 CreateScenarioButton.propTypes = {
+  /**
+   * Scenario selected in context
+   */
   currentScenario: PropTypes.object,
+  /**
+   * List of available scenarios
+   */
   scenarios: PropTypes.array.isRequired,
+  /**
+   * List of available datasets
+   */
   datasets: PropTypes.array.isRequired,
+  /**
+   * List of available scenario types
+   */
   runTemplates: PropTypes.array.isRequired,
+  /**
+   * User information (will be send for scenario creation)
+   */
   user: PropTypes.object.isRequired,
+  /**
+   * Function that create a scenario
+   */
   createScenario: PropTypes.func.isRequired,
+  /**
+   * Current workspace id
+   */
   workspaceId: PropTypes.string.isRequired,
+  /**
+   * Current solution
+   */
   solution: PropTypes.object.isRequired,
-  buttonTooltip: PropTypes.string.isRequired,
+  /**
+   *  Define the CreateScenarioButton's state:
+   *  - true : the button is disabled
+   *  - false : the button is enabled
+   */
   disabled: PropTypes.bool.isRequired,
+  /**
+   * RegExp used to validate scenario name
+   */
   nameValidator: PropTypes.instanceOf(RegExp),
-  dialogLabels: PropTypes.object
+  /**
+   * Structure:
+   * <pre>
+   *   {
+          button : {
+              title: 'string',
+              tooltip: 'string'
+          },
+          dialog: {
+            title: 'string',
+            scenarioName: 'string',
+            scenarioMaster: 'string',
+            scenarioParent: 'string',
+            datasetPlaceholder: 'string',
+            dataset: 'string',
+            scenarioTypePlaceholder: 'string',
+            scenarioType: 'string',
+            cancel: 'string',
+            create: 'string'
+          },
+          errors: {
+            emptyScenarioName:'string',
+            existingScenarioName:'string',
+            forbiddenCharsInScenarioName:'string'
+          }
+      }
+   * </pre>
+   */
+  labels: PropTypes.shape({
+    button: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      tooltip: PropTypes.string.isRequired
+    }).isRequired,
+    dialog: PropTypes.object.isRequired,
+    errors: PropTypes.shape({
+      emptyScenarioName: PropTypes.string.isRequired,
+      existingScenarioName: PropTypes.string.isRequired,
+      forbiddenCharsInScenarioName: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
 };
 
 CreateScenarioButton.defaultProps = {
