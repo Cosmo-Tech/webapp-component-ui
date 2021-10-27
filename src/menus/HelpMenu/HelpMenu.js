e086c5b3a5d7064e604bec019a0ab1410eb8e6d8
Nicolas Borde <nicolas.borde@cosmotech.com>
2021-10-27 10:40:25 +0200

    feat: add about button in help menu and some data-cy props

--- src/menus/HelpMenu/HelpMenu.js
+++ src/menus/HelpMenu/HelpMenu.js
@@ -1,6 +1,19 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Box, Menu, MenuItem, ClickAwayListener } from '@material-ui/core';
+import {
+  Box,
+  Menu,
+  MenuItem,
+  ClickAwayListener,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  Button,
+} from '@material-ui/core';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 import useStyles from './style';
 
@@ -8,9 +21,11 @@ export const HelpMenu = (props) => {
   const classes = useStyles();
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
+  const [showAboutDialog, setShowAboutDialog] = useState(false);
 
   const handleClose = () => {
     setMenuOpen(false);
+    setShowAboutDialog(false);
   };
 
   const handleClick = (event) => {
@@ -18,7 +33,7 @@ export const HelpMenu = (props) => {
     setMenuOpen(!isMenuOpen);
   };
 
-  const { documentationUrl, supportUrl, labels } = props;
+  const { documentationUrl, supportUrl, about, labels } = props;
 
   return (
     <ClickAwayListener onClickAway={handleClose}>
@@ -46,7 +61,30 @@ export const HelpMenu = (props) => {
               </a>
             </MenuItem>
           )}
+          {about && (
+            <MenuItem data-cy="about-button" onClick={() => setShowAboutDialog(true)}>
+              {labels.aboutTitle}
+            </MenuItem>
+          )}
         </Menu>
+        {about && (
+          <Dialog data-cy="about-dialog" open={showAboutDialog} onClose={handleClose}>
+            <DialogTitle>{labels.aboutTitle}</DialogTitle>
+            <DialogContent>{about}</DialogContent>
+            <DialogActions>
+              <Button
+                data-cy="about-dialog-close-button"
+                autoFocus
+                color="primary"
+                onClick={() => {
+                  handleClose();
+                }}
+              >
+                {labels.close}
+              </Button>
+            </DialogActions>
+          </Dialog>
+        )}
       </div>
     </ClickAwayListener>
   );
@@ -56,11 +94,15 @@ HelpMenu.propTypes = {
   /**
    * Documentation url
    */
-  documentationUrl: PropTypes.string,
+  documentationUrl: PropTypes.string.isRequired,
   /**
    * Support page url
    */
-  supportUrl: PropTypes.string,
+  supportUrl: PropTypes.string.isRequired,
+  /**
+   * About component
+   */
+  about: PropTypes.element,
   /**
    * Component's labels:
    * Structure:
@@ -68,19 +110,25 @@ HelpMenu.propTypes = {
    {
       documentation: 'string',
       suuport: 'string',
+      aboutTitle: 'string',
+      close: 'string',
     }
    *   </pre>
    */
   labels: PropTypes.shape({
-    documentation: PropTypes.string.isRequired,
+    documentation: PropTypes.string,
     support: PropTypes.string,
+    aboutTitle: PropTypes.string,
+    close: PropTypes.string,
   }),
 };
 
 HelpMenu.defaultProps = {
-  supportUrl: null,
   labels: {
     documentation: 'Documentation',
     support: 'Contact support',
+    aboutTitle: 'About',
+    close: 'Close',
   },
+  about: null,
 };
