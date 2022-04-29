57812b9c9be0d6fb5b9ad091c25b2f563251cd9d
esasova <sassova.elena@gmail.com>
2022-04-29 15:52:24 +0200

    fix:refactor value setters for table component

--- src/inputs/Table/ColumnTypes.js
+++ src/inputs/Table/ColumnTypes.js
@@ -14,16 +14,17 @@ const _editModeSetter = (params) => {
 
 const _boolSetter = (params) => {
   let newValue = params.newValue.toLowerCase();
+  const allowedEmptyFields = params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0;
   if (!params.context.editMode) {
     newValue = params.oldValue;
-  } else if (['0', 'false', 'no'].indexOf(newValue) !== -1) {
-    newValue = 'false';
-  } else if (['1', 'true', 'yes'].indexOf(newValue) !== -1) {
-    newValue = 'true';
-  } else if (params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0) {
-    newValue = '';
   } else {
-    newValue = params.oldValue;
+    if (['0', 'false', 'no'].indexOf(newValue) !== -1) {
+      newValue = 'false';
+    } else if (['1', 'true', 'yes'].indexOf(newValue) !== -1) {
+      newValue = 'true';
+    } else if (!allowedEmptyFields) {
+      newValue = params.oldValue;
+    }
   }
   params.data[params.colDef.field] = newValue;
   return true;
@@ -32,12 +33,16 @@ const _boolSetter = (params) => {
 const _dateSetter = (params) => {
   const dateFormat = params.context.dateFormat;
   let newValue = params.newValue;
-  if (params.context.editMode && params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0) {
-    newValue = '';
+  const allowedEmptyFields =
+    params.context.editMode && params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0;
+  if (allowedEmptyFields) {
     params.data[params.colDef.field] = newValue;
+    return true;
   } else {
     if (!params.context.editMode || !DateUtils.isValid(newValue, dateFormat)) {
       newValue = params.oldValue;
+      params.data[params.colDef.field] = newValue;
+      return true;
     } else {
       const minValue = params.column.userProvidedColDef?.cellEditorParams?.minValue;
       const maxValue = params.column.userProvidedColDef?.cellEditorParams?.maxValue;
@@ -47,18 +52,18 @@ const _dateSetter = (params) => {
       if (maxValue !== undefined) {
         newValue = DateUtils.strMin(newValue, maxValue, dateFormat) || params.oldValue;
       }
+      // Force date format before setting it
+      params.data[params.colDef.field] = DateUtils.format(DateUtils.parse(newValue, dateFormat), dateFormat);
+      return true;
     }
-    // Force date format before setting it
-    params.data[params.colDef.field] = DateUtils.format(DateUtils.parse(newValue, dateFormat), dateFormat);
   }
-  return true;
 };
 
 const _intSetter = (params) => {
   let newValue = params.newValue;
-  if (params.context.editMode && params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0) {
-    newValue = '';
-  } else {
+  const allowedEmptyFields =
+    params.context.editMode && params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0;
+  if (!allowedEmptyFields) {
     newValue = parseInt(params.newValue);
     if (!params.context.editMode || !Number.isSafeInteger(newValue)) {
       newValue = params.oldValue;
@@ -81,9 +86,9 @@ const _intSetter = (params) => {
 
 const _numberSetter = (params) => {
   let newValue = params.newValue;
-  if (params.context.editMode && params.colDef.cellEditorParams.acceptsEmptyFields && newValue.length === 0) {
-    newValue = '';
-  } else {
+  const allowedEmptyFields =
+    params.context.editMode && params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0;
+  if (!allowedEmptyFields) {
     newValue = parseFloat(params.newValue);
     if (!params.context.editMode || isNaN(newValue)) {
       newValue = params.oldValue;
@@ -109,9 +114,8 @@ const _enumSetter = (params) => {
     console.warn(`Missing enum values for table column "${params.column.colId}"`);
   }
   let newValue = params.newValue;
-  if (params.context.editMode && params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0) {
-    newValue = '';
-  } else if (!params.context.editMode || enumValues.indexOf(newValue) === -1) {
+  const allowedEmptyFields = params.colDef.cellEditorParams?.acceptsEmptyFields && newValue.length === 0;
+  if (!allowedEmptyFields && (!params.context.editMode || enumValues.indexOf(newValue) === -1)) {
     newValue = params.oldValue;
   }
   params.data[params.colDef.field] = newValue;
