5cd90b79bed227faea5f781bb4b7941787920991
Tristan Huet <tristan.huet@cosmotech.com>
2023-02-16 18:20:46 +0100

    test: remove jest tests asserting the value changes of `BasicSliderInput` component
    These tests should be restored in the future when we find a way
    to mock the mouse events interacting the mui Slider component.
    In the meantime, the slider behavior is still tested in cypress
    tests in the azure-sample-webapp repository.

--- src/inputs/BasicInputs/BasicSliderInput/BasicSliderInput.spec.js
+++ src/inputs/BasicInputs/BasicSliderInput/BasicSliderInput.spec.js
@@ -37,18 +37,6 @@ const propsInEditMode = {
   ...defaultProps,
   disabled: false,
 };
-const propsWithNullValueEditMode = {
-  ...propsInEditMode,
-  value: null,
-};
-const propsWithUndefinedValueEditMode = {
-  ...propsInEditMode,
-  value: undefined,
-};
-const propsWithNaNValueEditMode = {
-  ...propsInEditMode,
-  value: NaN,
-};
 
 const sliderDisabled = new TextFieldTesting({ dataCy: 'slider-input-textField' });
 const sliderEditMode = new SliderTesting({ dataCy: 'slider-input' });
@@ -89,11 +77,6 @@ describe('BasicSliderInput tests in disabled and edit mode', () => {
       expect(sliderEditMode.SliderMarksValue[0]).toEqual(propsInEditMode.min.toString());
       expect(sliderEditMode.SliderMarksValue[1]).toEqual(propsInEditMode.max.toString());
     });
-
-    test('Checks firing onChange function with right values while moving slider', async () => {
-      await sliderEditMode.moveSliderToTheRight(mockOnValueChanged, 6);
-      await sliderEditMode.moveSliderToTheLeft(mockOnValueChanged, 4);
-    });
   });
 
   describe('Checks slider value if provided one is null, undefined or NaN', () => {
@@ -112,24 +95,6 @@ describe('BasicSliderInput tests in disabled and edit mode', () => {
     test('Checks textField value is 0 when NaN is provided', () => {
       setUp(propsWithNaNValue);
       expect(sliderDisabled.TextFieldValue).toHaveValue('0');
-    });
-
-    test('Checks slider value is 0 and handleChange function is working when null value is provided', async () => {
-      setUp(propsWithNullValueEditMode);
-      expect(sliderEditMode.SliderValue).toHaveValue('0');
-      await sliderEditMode.moveSliderToTheRight(mockOnValueChanged, 1);
-    });
-
-    test('Checks slider value is 0 and handleChange function is working when undefined value is provided', async () => {
-      setUp(propsWithUndefinedValueEditMode);
-      expect(sliderEditMode.SliderValue).toHaveValue('0');
-      await sliderEditMode.moveSliderToTheRight(mockOnValueChanged, 1);
-    });
-
-    test('Checks slider value is 0 and handleChange function is working when NaN value is provided', async () => {
-      setUp(propsWithNaNValueEditMode);
-      expect(sliderEditMode.SliderValue).toHaveValue('0');
-      await sliderEditMode.moveSliderToTheRight(mockOnValueChanged, 1);
     });
   });
 });
