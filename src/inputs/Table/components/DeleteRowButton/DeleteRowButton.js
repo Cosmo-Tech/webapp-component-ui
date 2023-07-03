f94693075a1a98a95fd2214145752bacdb997aa7
Guillaume Boudaille <guillaume.boudaille@epitech.eu>
2023-07-03 15:33:24 +0200

    fix: fixup

--- src/inputs/Table/components/DeleteRowButton/DeleteRowButton.js
+++ src/inputs/Table/components/DeleteRowButton/DeleteRowButton.js
@@ -1,28 +1,48 @@
 // Copyright (c) Cosmo Tech.
 // Licensed under the MIT license.
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { IconButton } from '@mui/material';
 import PropTypes from 'prop-types';
 import { FadingTooltip } from '../../../../misc';
+import { DontAskAgainDialog } from '../../../../dialogs';
 
 export const DeleteRowButton = (props) => {
   const { onDeleteRow, label, disabled } = props;
+  const [isOpen, setOpen] = useState(false);
+  const switchDontAskAgainToDeleteRow = useCallback((isChecked) => {
+    localStorage.setItem('dontAskAgainToDeleteRow', isChecked);
+  }, []);
+  const switchOpenValue = useCallback(() => {
+    localStorage.getItem('dontAskAgainToDeleteRow') === 'true' ? onDeleteRow() : setOpen(!isOpen);
+  }, [isOpen, onDeleteRow]);
   return (
-    <FadingTooltip title={label} useSpan={true}>
-      <IconButton
-        onClick={onDeleteRow}
-        component="label"
-        size="small"
-        color="primary"
-        data-cy="delete-rows-button"
-        disabled={disabled}
-        variant="text"
-      >
-        <DeleteIcon fontSize="inherit" />
-      </IconButton>
-    </FadingTooltip>
+    <>
+      <FadingTooltip title={label} useSpan={true}>
+        <IconButton
+          onClick={switchOpenValue}
+          component="label"
+          size="small"
+          color="primary"
+          data-cy="delete-rows-button"
+          disabled={disabled}
+          variant="text"
+        >
+          <DeleteIcon fontSize="inherit" />
+        </IconButton>
+      </FadingTooltip>
+      <DontAskAgainDialog
+        id="dontAskAgainDeleteRow"
+        onClose={switchOpenValue}
+        onConfirm={(isChecked) => {
+          switchOpenValue();
+          switchDontAskAgainToDeleteRow(isChecked);
+          onDeleteRow();
+        }}
+        open={isOpen}
+      ></DontAskAgainDialog>
+    </>
   );
 };
 
