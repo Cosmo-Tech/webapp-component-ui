2ce4962986ca58b846f287af9f2a91c4e59a8b54
Guillaume Boudaille <guillaume.boudaille@epitech.eu>
2023-07-04 16:14:59 +0200

    feat: Add import/export on toolbar

---
+++ src/inputs/Table/components/ImportButton/ImportButton.js
@@ -0,0 +1,43 @@
+// Copyright (c) Cosmo Tech.
+// Licensed under the MIT license.
+
+import React from 'react';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import { IconButton } from '@mui/material';
+import PropTypes from 'prop-types';
+import { FadingTooltip } from '../../../../misc';
+
+export const ImportButton = (props) => {
+  const { disabled, onImport, label } = props;
+  return (
+    <FadingTooltip title={label} useSpan={true}>
+      <IconButton
+        variant="text"
+        component="label"
+        onChange={onImport}
+        size="small"
+        color="primary"
+        disabled={disabled}
+        data-cy="import-file-button"
+      >
+        <input type="file" accept=".csv, .xlsx" hidden />
+        <UploadFileIcon fontSize="inherit" />
+      </IconButton>
+    </FadingTooltip>
+  );
+};
+
+ImportButton.propTypes = {
+  /*
+   * Function used to import a file in Table component readable by AgGrid
+   */
+  onImport: PropTypes.func.isRequired,
+  /*
+   * Boolean that defines whether button is disabled
+   */
+  disabled: PropTypes.bool.isRequired,
+  /*
+   * Label of the button, displayed as a tooltip
+   */
+  label: PropTypes.string.isRequired,
+};
