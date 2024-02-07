50c763aea2cc1b2044a414d3e018ffa2c10db1cb
Tristan Huet <tristan.huet@cosmotech.com>
2024-02-07 15:57:53 +0100

    chore: remove generated components documentation

--- doc/doczrc.js
+++
@@ -1,4 +0,0 @@
-export default {
-  base: '/webapp-component-ui',
-  description: 'Webapp UI components library documentation',
-};
--- doc/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  flags: {
-    FAST_DEV: true,
-    FAST_REFRESH: true,
-    DEV_SSR: false,
-  },
-};
