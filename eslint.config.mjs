8a97d047321b24d559cd75001b84c26baa29009a
Elena Sasova <sassova.elena@gmail.com>
2024-10-08 11:03:22 +0200

    build: [PROD-13948] replace eslint standard by neostandard package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,68 +1,74 @@
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import prettier from "eslint-plugin-prettier";
-import jest from "eslint-plugin-jest";
-import { fixupPluginRules } from "@eslint/compat";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { fixupPluginRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import jest from 'eslint-plugin-jest';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import neostandard from 'neostandard';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const neostandardConfig = neostandard({ semi: true, noStyle: true });
+
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/build", "**/dist", "**/.docz", "**/.github", "**/node_modules"],
-}, ...compat.extends(
-    "plugin:react/recommended",
-    "standard",
-    "eslint:recommended",
-    "prettier",
-    "plugin:prettier/recommended",
-), {
+export default [
+  {
+    ignores: ['**/build', '**/dist', '**/.docz', '**/.github', '**/node_modules'],
+  },
+  ...neostandardConfig,
+  ...compat.extends('plugin:react/recommended', 'prettier', 'plugin:prettier/recommended'),
+  {
     plugins: {
-        react,
-        "react-hooks": fixupPluginRules(reactHooks),
-        prettier,
-        jest,
+      react,
+      'react-hooks': fixupPluginRules(reactHooks),
+      prettier,
+      jest,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...jest.environments.globals.globals,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...jest.environments.globals.globals,
+      },
 
-        ecmaVersion: 12,
-        sourceType: "module",
+      ecmaVersion: 12,
+      sourceType: 'module',
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
 
     settings: {
-        react: {
-            version: "18",
-        },
+      react: {
+        version: '18',
+      },
     },
 
     rules: {
-        "no-constant-binary-expression": "error",
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        semi: [2, "always"],
+      'no-constant-binary-expression': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      semi: [2, 'always'],
 
-        "max-len": ["error", {
-            code: 120,
-        }],
+      'max-len': [
+        'error',
+        {
+          code: 120,
+        },
+      ],
     },
-}];
+  },
+];
