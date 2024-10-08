d9badfae9b323f514015a24c6398b52d44a5576f
Elena Sasova <sassova.elena@gmail.com>
2024-10-08 11:04:03 +0200

    build: [PROD-13948] delete not supported asset keyword- from rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,13 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
+import resolve from '@rollup/plugin-node-resolve';
+import { readFileSync } from 'fs';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import resolve from '@rollup/plugin-node-resolve';
-import image from '@rollup/plugin-image';
 import { visualizer } from 'rollup-plugin-visualizer';
-import pkg from './package.json' assert { type: 'json' };
+
+const pkg = JSON.parse(readFileSync('./package.json'));
 
 export default {
   onwarn: (warning, warn) => {
